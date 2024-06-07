5ab82f9b522ae2f3d38e66c159ab862939d859dc
PML30\cgsg202 <e.pavlov@ad.pml30.ru>
2024-06-07 16:33:40 +0300

    Added new folder with files.

---
+++ 0606/main.js
@@ -0,0 +1,29 @@
+import * as mth from "./math.js";
+
+function main() {
+  const element = document.querySelector("#glcanvas");
+  const gl = element.getContext("webgl2");
+
+  if (gl === null) {
+    alert("WebGL2 not supported");
+    return;
+  }
+
+  gl.clearColor(...mth.getClearColor());
+  // gl.clearColor(1, 0, 0, 1);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  
+  const anim = () => {
+    gl.clearColor(1, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    window.requestAnimationFrame(anim);
+  };
+
+  anim();
+}
+
+window.addEventListener("load", () => {
+  main();
+});
---
+++ 0606/math.js
@@ -0,0 +1,5 @@
+export * as vec3 from 'vec3.js'
+
+export function getClearColor() {
+    return [0, 1, 0, 1];
+}
